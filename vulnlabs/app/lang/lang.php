26e8b3db17f4611ce505024322e6bfedafae12a8
Blas <blasillous@gmail.com>
2024-09-02 11:41:29 +0200

    vulnlab es

---
+++ vulnlabs/app/lang/lang.php
@@ -0,0 +1,59 @@
+<?php
+
+function getLang() {
+
+$langs = array("en", "fr", "es");
+
+if(!isset($_COOKIE["lang"])) {
+    $lang = "en";
+} else {
+    if(in_array($_COOKIE["lang"],$langs)) {
+        $lang = $_COOKIE["lang"];
+    } else {
+        $lang = "en";
+    }
+    
+}
+
+return $lang;
+
+}
+
+function getLangName($langCode) {
+    switch ($langCode) {
+        case "en":
+            return "English";
+        case "tr":
+            return "Türkçe";
+        case "fr":
+            return "Français";
+        case "ar":
+            return "عربي";
+        case "es":
+            return "Castellano";
+        }
+        
+}
+
+function tr($path = NULL)
+{
+
+
+$lang = getLang();
+
+if(!isset($path)) {
+    $path = "./$lang.ini";
+} else {
+    $path = "$path/$lang.ini";
+
+}
+
+$strings = parse_ini_file($path);
+return $strings;
+
+
+}
+
+
+
+?>
